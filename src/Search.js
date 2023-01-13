a93a6998978b0b503c1adaa65da63502b256b793
Flydeer19 <114246352+Flydeer19@users.noreply.github.com>
2023-01-12 19:53:57 -0700

    removed axios

--- src/Search.js
+++ src/Search.js
@@ -1,10 +1,6 @@
 import React from "react";
-import { AxiosContext } from "react-axios/lib/components/AxiosProvider";
 
 export default function Search() {
-  //   const apiKey = "8ca7dd4e61360b90fb66918853670e48";
-  //   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${apiKey}&units=metric`;
-  //   axios.get(apiUrl).then(showCity);
   return (
     <form className="Search">
       <input type="search" />
