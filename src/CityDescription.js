99eaabff1d8540cdf66a7a7af2b2b049016e014d
Flydeer19 <114246352+Flydeer19@users.noreply.github.com>
2023-01-12 18:29:43 -0700

    added basic app structure

---
+++ src/CityDescription.js
@@ -0,0 +1,14 @@
+import react from "react";
+import "./App.css";
+
+export default function CityDescription() {
+  return (
+    <div className="CityDescription">
+      <ul>
+        <li className="cityName">Kyiv</li>
+        <li>Friday 18:20</li>
+        <li>Clouds</li>
+      </ul>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import Search from "./Search";
+import CityDescription from "./CityDescription";
+import "./App.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div className="appBox container">
+    <React.StrictMode>
+      <Search />
+      <CityDescription />
+      <div className="container">
+        <div className="row">
+          <div className="col"></div>
+        </div>
+        <div className="row">
+          <div className="col"></div>
+        </div>
+      </div>
+    </React.StrictMode>
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
